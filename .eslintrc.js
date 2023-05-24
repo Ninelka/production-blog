5ea4838a9ffcccb5be7525076a7d6053ddc133fb
Ninel <ninelgruzdeva@gmail.com>
2023-05-24 23:49:10 +0500

    fix: build

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,6 @@ module.exports = {
     sourceType: 'module',
     project: ['./tsconfig.json']
   },
-  plugins: [
-    'react',
-    'i18next'
-  ],
   rules: {
     '@typescript-eslint/strict-boolean-expressions': 0,
     '@typescript-eslint/prefer-nullish-coalescing': 0,
@@ -24,10 +20,15 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': 1,
     '@typescript-eslint/explicit-function-return-type': 1,
     'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
     'react/display-name': 1,
     'no-tabs': 0,
     'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }]
   },
+  plugins: [
+    'react',
+    'i18next'
+  ],
   globals: {
     __IS_DEV__: true
   },

23f4962c592f395c5c277bc153b6b2ec11eec195
Ninel <ninelgruzdeva@gmail.com>
2023-05-25 12:18:55 +0500

    fix build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -13,13 +13,13 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev)
     })
   ]
 
   if (isDev) {
-    plugins.push(new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
-    }))
     plugins.push(new BundleAnalyzerPlugin({
       openAnalyzer: false
     }))

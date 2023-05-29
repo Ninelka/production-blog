45ba288bf019ef2ff4751703272fead1fdd5fc93
Ninel <ninelgruzdeva@gmail.com>
2023-05-29 22:07:57 +0500

    add redux store, add first entity

---
+++ src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -0,0 +1,14 @@
+import { type DeepPartial } from 'redux'
+import { type StateSchema } from 'app/providers/StoreProvider'
+import { getCounter } from './getCounter'
+
+describe('getCounter', () => {
+  test('should return counter value', () => {
+    const state: DeepPartial<StateSchema> = {
+      counter: {
+        value: 10
+      }
+    }
+    expect(getCounter(state as StateSchema)).toEqual({ value: 10 })
+  })
+})

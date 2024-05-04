74aa379d68bcf3c36369e60c0f364e84c8466fa7
Ninel <ninelgruzdeva@gmail.com>
2024-05-04 19:34:00 +0500

    add tests for isolated components with cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents () {},
     baseUrl: 'http://localhost:3000/'
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack'
+    }
   }
 })

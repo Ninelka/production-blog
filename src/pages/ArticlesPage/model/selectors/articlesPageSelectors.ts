17a04947f836ca1a13de83970d7d8e4d70ab2032
Ninel <ninelgruzdeva@gmail.com>
2024-05-11 23:49:01 +0500

    add argument for selector

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -1,5 +1,6 @@
 import { type StateSchema } from '@/app/providers/StoreProvider'
 import { ArticleType, ArticleView } from '@/entities/Article'
+import { buildSelector } from '@/shared/lib/store'
 
 export const getArticlesPageIsLoading = (state: StateSchema) =>
     state.articlesPage?.isLoading || false
@@ -17,3 +18,7 @@ export const getArticlesPageInited = (state: StateSchema) =>
     state.articlesPage?._inited
 export const getArticlesPageType = (state: StateSchema) =>
     state.articlesPage?.type ?? ArticleType.ALL
+
+export const [useArticleItemById] = buildSelector(
+    (state, id: string) => state.articlesPage?.entities?.[id],
+)

724d2b1d10b3dac0e63ae347dcdce008aa08db5a
Ninel <ninelgruzdeva@gmail.com>
2024-05-22 02:20:20 +0500

    fix storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -5,27 +5,29 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator'
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from '../../src/shared/const/theme'
+import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator'
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
-    }
-  },
-  layout: 'fullscreen',
-  themes: {
-    default: 'light',
-    list: [
-      { name: 'light', class: Theme.LIGHT, color: '#fff' },
-      { name: 'dark', class: Theme.DARK, color: '#000' },
-      { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
-    ]
-  }
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#fff' },
+            { name: 'dark', class: Theme.DARK, color: '#000' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+        ],
+    },
 }
 
 addDecorator(StyleDecorator)
 addDecorator(ThemeDecorator(Theme.LIGHT))
 addDecorator(RouterDecorator)
 addDecorator(SuspenseDecorator)
+addDecorator(StoreDecorator({ user: {} }))
